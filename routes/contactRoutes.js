93408a898245e107fd626d0de69821931f430aef
toukirislam <shovoodev@gmail.com>
2023-12-25 15:30:15 +0100

    maneging routes for project

---
+++ routes/contactRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+
+router.route("/").get((req, res) => {
+    res.status(200).json({message : "Get all contacts"});
+})
+router.route("/").post((req, res) => {
+    res.status(200).json({message : "create contact"});
+})
+router.route("/:id").put((req, res) => {
+    res.status(200).json({message : ` update contact for ${req.params.id}`});
+})
+router.route("/:id").delete((req, res) => {
+    res.status(200).json({message : `delete contacts for  ${req.params.id}`});
+})
+module.exports = router ; 
